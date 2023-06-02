fabe0ed23e9c8692df251766e9ee732730ceffdd
lidevlin <lidevlin95@gmail.com>
2023-06-02 21:40:18 +0100

    Improved about page

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,5 +1,4 @@
 import { A } from "solid-start";
-import Counter from "~/components/Counter";
 
 export default function About() {
   return (
@@ -7,17 +6,8 @@ export default function About() {
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
         About Page
       </h1>
-      <Counter />
       <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
+        A simple app to display chess player's information and past games.
       </p>
       <p class="my-4">
         <A href="/" class="text-sky-600 hover:underline">
